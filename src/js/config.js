1657c90f9a8476cfebab9b4694c4a38770c49330
Pedro Vinicius Vale7 <163039257+PedroVinicins@users.noreply.github.com>
2025-02-27 21:59:56 -0300

    Atualiza configuração do Firebase para usar o Realtime Database e remove a referência à autenticação

--- src/js/config.js
+++ src/js/config.js
@@ -1,19 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBaz-T1o0EutMFxKWl8K8EbgWSV3A9U1aA",
-  authDomain: "notanet-34cad.firebaseapp.com",
-  projectId: "notanet-34cad",
-  storageBucket: "notanet-34cad.appspot.com",
-  messagingSenderId: "158464923311",
-  appId: "1:158464923311:web:431bee0f905d0a8334265a",
-  measurementId: "G-P4B2CH837M"
+    apiKey: "AIzaSyBaz-T1o0EutMFxKWl8K8EbgWSV3A9U1aA",
+    authDomain: "notanet-34cad.firebaseapp.com",
+    databaseURL: "https://notanet-34cad-default-rtdb.firebaseio.com",
+    projectId: "notanet-34cad",
+    storageBucket: "notanet-34cad.firebasestorage.app",
+    messagingSenderId: "158464923311",
+    appId: "1:158464923311:web:431bee0f905d0a8334265a",
+    measurementId: "G-P4B2CH837M",
 };
 
-// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const database = getDatabase(app);
 
-// Exporta o objeto auth
-export { auth };
+export { database };
